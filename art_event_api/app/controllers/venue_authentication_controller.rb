8c442c5670e797e935c5a97b601abfc0efc6721e
ameliadz <ameliazecker@gmail.com>
2019-06-04 21:49:31 -0400

    auth controller

--- art_event_api/app/controllers/venue_authentication_controller.rb
+++ art_event_api/app/controllers/venue_authentication_controller.rb
@@ -1,2 +1,17 @@
 class VenueAuthenticationController < ApplicationController
+  # POST /auth/login
+  def login
+
+    @venue_owner = VenueOwner.find_by(email: params[:email])
+    if !@user
+      render json: { error: 'no account' }, status: :not_found
+    end
+
+    if @venue_owner.authenticate(params[:password]) #authenticate method provided by Bcrypt and 'has_secure_password'
+      token = JsonWebToken.encode(venue_owner_id: @venue_owner.id, email: @venue_owner.email)
+      render json: { token: token }, status: :ok
+    else
+      render json: { error: 'unauthorized' }, status: :unauthorized
+    end
+  end
 end
