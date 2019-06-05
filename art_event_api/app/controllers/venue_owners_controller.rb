54b079b18636b9a17119d4705121d455c3ee5d98
ameliadz <ameliazecker@gmail.com>
2019-06-04 21:52:23 -0400

    venue owner controller

---
+++ art_event_api/app/controllers/venue_owners_controller.rb
@@ -0,0 +1,25 @@
+class VenueOwnersController < ApplicationController
+  before_action :authorize_request, except: :create
+
+  def index
+    @venue_owners = VenueOwner.all
+    render json: @venue_owners, status: :ok
+  end
+
+  def show
+  end
+
+  def create
+    @venue_owner = VenueOwner.new(venue_owner_params)
+    if @venue_owner.save
+      render json: @venue_owner, status: :created
+    else
+      render json: @venue_owner.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def venue_owner_params
+    params.require(:venue_owner).permit(:email, :password)
+  end
+end
